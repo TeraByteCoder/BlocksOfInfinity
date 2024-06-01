0604d4e981ddce60029c6779078f2a3e2257f01e
Lukas <lukas.hamburger@mail.ru>
2024-06-01 10:38:38 +0200

    Initial commit

---
+++ html/src/at/lukas/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package at.lukas.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import at.lukas.game.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Main();
+        }
+}
